77c5cd1d4b7b42f470c63b5054504d1e56747758
keren ramos <keren.ramoss@gmail.com>
2023-01-30 17:57:19 -0600

    pasar js a vue

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const buttonEl = document.querySelector('button');
+//transformacion de js a vue
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      goal: "",
+    };
+  },
+  methods: {
+    addGoals() {
+      if (this.goal != "") {
+        this.goals.push(this.goal);
+        this.goal = "";
+      }
+    },
+  },
+}).mount("#app");
+
+/*const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
@@ -10,4 +28,4 @@ function addGoal() {
   inputEl.value = '';
 }
 
-buttonEl.addEventListener('click', addGoal);
+buttonEl.addEventListener('click', addGoal);*/
